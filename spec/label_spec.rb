a8e570891ae325be337a9f1fcdf76e6816b0bc25
Christopher Uduekwe <christopheruduekwe@gmail.com>
2023-10-17 16:53:21 +0100

    Add unit tests for all implemented methods.

---
+++ spec/label_spec.rb
@@ -0,0 +1,20 @@
+# label_spec.rb
+require 'rspec'
+require_relative '../classes/label'
+
+describe Label do
+  let(:label) { Label.new('Sample Label', 'Blue') }
+  let(:item) { Item.new(1, Date.new(2020, 1, 1)) }
+
+  it 'initializes with a name and color' do
+    expect(label.name).to eq('Example Label')
+    expect(label.color).to eq('Blue')
+  end
+
+  it 'adds an item and sets the label property on the item' do
+    label.add_item(item)
+
+    expect(label.items).to include(item)
+    expect(item.label).to eq(label)
+  end
+end
