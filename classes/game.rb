ce7a3f7ab63e464b88ffee1babe8de037da8ae80
Fatima Najafi <najafifatima762@gmail.com>
2023-10-18 15:40:15 +0430

    updade game.rb

--- classes/game.rb
+++ classes/game.rb
@@ -4,7 +4,7 @@ class Game < Item
   attr_accessor :multiplayer, :last_played_at, :author
 
   def initialize(id, publish_date, multiplayer, last_played_at)
-    super(id, publish_date)
+    super(publish_date, id: id)
     @multiplayer = multiplayer
     @last_played_at = last_played_at
     @author = author
