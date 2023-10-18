de02c99d443197403767a4dee8ec63f169a7e55c
Fatima Najafi <najafifatima762@gmail.com>
2023-10-18 13:07:43 +0430

    Update game class

--- classes/game.rb
+++ classes/game.rb
@@ -3,7 +3,7 @@ require_relative 'item'
 class Game < Item
   attr_accessor :multiplayer, :last_played_at, :author
 
-  def initialize(id, publish_date, multiplayer, last_played_at, author)
+  def initialize(id, publish_date, multiplayer, last_played_at)
     super(id, publish_date)
     @multiplayer = multiplayer
     @last_played_at = last_played_at
